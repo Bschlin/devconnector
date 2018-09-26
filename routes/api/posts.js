3b725929e9058d3625d6d698c83a8823c41ec0b9
Blake Schlin <blakeschlin@gmail.com>
2018-09-26 15:47:07 -0500

    add additional routes for posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,115 @@
 const express = require("express");
-
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Load Post model
+const Post = require("../../models/Post");
+// Load Profile model
+const Profile = require("../../models/Profile");
+
+// Validation
+const validatePostInput = require("../../validation/post");
 
 // @route   GET api/posts/test
 // @desc    Tests post route
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Posts works" }));
+
+// @route   GET api/posts
+// @desc    Get posts
+// @access  Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: "desc" }) // sort posts by date in descending order starting with most recent
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostsfound: "No posts found" }));
+});
+
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => {
+      if (post) {
+        res.json(post);
+      } else {
+        res.status(404).json({ nopostfound: "No post found with that ID" });
+      }
+    })
+    .catch(err =>
+      res.status(404).json({ nopostfound: "No post found with that ID" })
+    );
+});
+
+// @route   POST api/posts
+// @desc    Create post
+// @access  Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // If any errors, send 400 with errors object
+      return res.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+);
+
+// @route   DELETE api/posts/:id
+// @desc    Delete post
+// @access  Private
+
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    // Check Id of owner post
+    Post.findOneAndRemove({ _id: req.params.id, user: req.user.id })
+      .then(post => {
+        return !post
+          ? res.status(401).json({ post: "post not found" })
+          : res.status(200).json({ post: "post deleted" });
+      })
+      .catch(err =>
+        res.status(404).json({ post: "It had a problem deleting the post" })
+      );
+  }
+);
+// router.delete(
+//   "/:id",
+//   passport.authenticate("jwt", { session: false }),
+//   (req, res) => {
+//     // profile is used to test if the current user is the same one that made the post. If it isn't, we don't allow deletion.
+//     Profile.findOne({ user: req.user.id }).then(profile => {
+//       Post.findById(req.params.id)
+//         .then(post => {
+//           // Check for post owner
+//           if (post.user.toString() !== req.user.id) {
+//             return res
+//               .status(401)
+//               .json({ notauthorized: "User not authorized" });
+//           }
+
+//           // Delete
+//           post.remove().then(() => res.json({ success: true }));
+//         })
+//         .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+//     });
+//   }
+// );
 
 module.exports = router;
