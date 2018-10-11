9d46b625596545c7d1a1814c5de85b7f9dc25bc5
Blake Schlin <blakeschlin@gmail.com>
2018-10-11 14:31:11 -0500

    add redux logout and navbar functionality

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
-import { setCurrentUser } from "./actions/authActions";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 // provides are app with the store which wraps state and components
 import { Provider } from "react-redux";
 import store from "./store";
@@ -23,6 +23,16 @@ if (localStorage.jwtToken) {
   const decoded = jwt_decode(localStorage.jwtToken);
   // set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
+
+  // check for expired token
+  const currenTime = Date.now() / 1000;
+  if (decoded.exp < currenTime) {
+    // logout user
+    store.dispatch(logoutUser());
+    // TO DO: clear current profile
+    // redirect to login
+    window.location.href = "/login";
+  }
 }
 
 class App extends Component {
