2c9fb59df56758934817366f62d9a48dba99f232
Blake Schlin <blakeschlin@gmail.com>
2019-07-07 15:57:54 -0500

    test

---
+++ config/keys_prod.js
@@ -0,0 +1,5 @@
+// makes module available outside this file
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY
+};
--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose
   .catch(err => console.log(err));
 
 // route with callback, takes in two parameters
-app.get("/", (req, res) => res.send("Hello derr"));
+
 
 // passport middleware
 app.use(passport.initialize());
@@ -49,6 +49,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
